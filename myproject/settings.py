3ecd8903b8f73324b0703ffc4938da0af7183aea
Karthik31102005-tech <karthik31102005@gmail.com>
2025-04-05 21:43:01 +0530

    debug clear

--- myproject/settings.py
+++ myproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-o#4&k=tf-r4qh#2pt%)^q19x%ga1$5uf+4v&je9afi^_!^)1gc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
